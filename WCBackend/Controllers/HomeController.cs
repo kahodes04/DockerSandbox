8f441bda4cca2cfddb309feb88026755b1dad6f5
Guillermo von Stolzmann <guillin_04_97@hotmail.com>
2022-10-11 23:01:36 +0200

    add cors

--- WCBackend/Controllers/HomeController.cs
+++ WCBackend/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using WCBackend.model;
@@ -6,6 +7,7 @@ using WCBackend.Models;
 
 namespace WCBackend.Controllers
 {
+    [EnableCors("NUXT")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
